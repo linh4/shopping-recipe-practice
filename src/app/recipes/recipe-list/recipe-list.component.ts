c10141e5edacfb2ee47c859e95e72e6666bc9b66
linh4 <linh44h@gmail.com>
2019-04-29 18:26:24 -0400

    added events for showing recipes and add new ingredient

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../../shared/recipe.model';
 
 @Component({
@@ -7,13 +7,20 @@ import { Recipe } from '../../shared/recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
   recipes: Recipe[] = [
-    new Recipe('Test', 'desc for test', 'https://downshiftology.com/wp-content/uploads/2018/10/Deviled-Eggs-Recipe.jpg')
+    new Recipe('Test-1', 'desc for test', 'https://downshiftology.com/wp-content/uploads/2018/10/Deviled-Eggs-Recipe.jpg'),
+    new Recipe('Test-2', 'desc for test', 'https://downshiftology.com/wp-content/uploads/2018/10/Deviled-Eggs-Recipe.jpg')
   ];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
