a4aa32431f949fca25d727aa6b23372dc6c9ba03
linh4 <linh44h@gmail.com>
2019-05-01 00:25:20 -0400

    added shopping list service

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,15 +1,38 @@
-import { EventEmitter } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
 import { Recipe } from '../shared/recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
+@Injectable()
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe('Test-1', 'desc for test', 'https://downshiftology.com/wp-content/uploads/2018/10/Deviled-Eggs-Recipe.jpg'),
-    new Recipe('Test-2', 'desc for test', 'https://downshiftology.com/wp-content/uploads/2018/10/Deviled-Eggs-Recipe.jpg')
+    new Recipe(
+      'Chicken Curry',
+      'Japanese tasty chicken curry',
+      'https://www.justonecookbook.com/wp-content/uploads/2013/03/Simple-Chicken-Curry.jpg',
+      [
+        new Ingredient('Chicken', 2),
+        new Ingredient('Carrot', 3)
+      ]),
+    new Recipe(
+      'Eggplant Parmesan',
+      'Perfect Italian Night',
+      'https://www.simplyrecipes.com/wp-content/uploads/2014/08/eggplant-parmesan-horiz-b-2000.jpg',
+      [
+        new Ingredient('Eggplant', 1),
+        new Ingredient('Parmesan', 4)
+      ])
   ];
+
+  constructor(private slService: ShoppingListService) {}
 
   getRecipes() {
     return this.recipes.slice();
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.slService.addIngredients(ingredients);
   }
 }
